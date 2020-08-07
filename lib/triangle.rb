4b3788e555fa2194f64d03a1f891021e8e6512a8
Warda Ayaz <wayaz107@gmail.com>
2020-08-07 08:57:09 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -25,12 +25,6 @@ class Triangle
    end
  end
 
-# def validate_triangle
-#   triangle = [(a + b > c),(a + c > b),(b + c > a)]
-#   [a, b, c].each {|x| triangle << false if x <= 0}
-# raise TriangleError if triangle.include?(false)
-# end
-
   class TriangleError < StandardError
   end
 
